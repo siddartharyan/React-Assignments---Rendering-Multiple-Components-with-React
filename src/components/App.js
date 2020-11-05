83c757cb6f443fc1e3405a7e49278727c197743b
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-11-05 12:22:25 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <div>
+        <h2 data-ns-test="project-name">Animals</h2>
+        <p data-ns-test="project-description">Cow, Lion, Tiger</p>
+      </div>
+      <div>
+        <h2 data-ns-test="project-name">Pets</h2>
+        <p data-ns-test="project-description">Dog, Cat</p>
+      </div>
+    </>
+  );
+};
 
 export default App;
